2a808eee995924a5f77d554a356b5f4b7f8d8e81
Alunos <aluno.etec@etecEmbu.com.br>
2022-09-19 21:55:49 -0300

    estilizaçao

---
+++ JS/operadores.js
@@ -0,0 +1,87 @@
+//exemplo de soma
+// let resultado = n1 + n2;
+// console.log("NUMÉRO 1 "+n1);
+// console.log("NUMÉRO 2 "+n2);
+// console.log("RESULTADO " +resultado);
+// console.log("");
+
+// //exemplo subtração
+// let n3 = 5;
+// let n4 = 8;
+// let subtração = n3 - n4;
+// console.log("NUMÉRO 3 "+n3);
+// console.log("NUMÉRO 4 "+n4);
+// console.log("RESULTADO " +subtração);
+// console.log("");
+
+// //exemplo multiplicação
+// let n5 = 3;
+// let n6 = 3;
+// let multiplicação = n5 * n6;
+// console.log("NUMÉRO 5 "+n5);
+// console.log("NUMÉRO 6 "+n6);
+// console.log("RESULTADO " + multiplicação);
+// console.log("");
+
+// console.log(n1>n2) // maior
+// console.log(n1>=n2)// maior ou igual
+// console.log(n1<n2) // menor
+// console.log(n1<=n2)// menor ou igual
+// console.log(n1==n2)// igual em valor
+// console.log(n1!=n2)// diferente em valor
+// console.log(n1===n2)// igualdade de tipo de dado e valor
+// console.log(n1!==n2)// diferença de tipo de dado e valor-
+// (n1<n2) && (n1>n2) E
+// (n1<n2) || (n1>n2) OU
+// (n1<n2) ! (n1>n2) negação
+
+// let peso = 125;
+
+// if(peso<=30){
+
+//   alert('Peso: LEVE');
+// }
+// else if(peso>=31 && peso<=70)
+// {
+//   alert('Peso: NORMAL');
+
+// }
+// else if(peso>=71 && peso<=100)
+// {
+//   alert('Peso: SOBREPESO');
+// }
+// else
+// {
+//   alert('Peso: MUITO PESADO')
+// }
+
+
+// if(idadeDoCarro < 2000){
+//   alert("Este carro é antigo");
+// }
+// else if((idadeDoCarro >=2000) && (idadeDoCarro <=2015))
+// {
+//   alert("Este carro é intermediário")
+// }
+// else{
+//   alert("Este carro é novo")
+// }
+
+// if(idadeDoCarro<=2000)
+// {
+//   alert('Seu carro é antigo');
+// }
+// else if(idadeDoCarro>=2015){
+//   alert('Seu carro é novo')
+// }
+// else{
+//   alert('Seu carro é intermediário')
+// }
+
+
+// if(idade>=18){
+//   alert("Você é maior de idade")
+// }
+// else{
+// alert("Você é menor de idade"+ "\n" + "saia")
+// }
